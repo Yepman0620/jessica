8c76fcb92cb3dd7158e567ded6c4703169fdc4dd
Yiwen <447411849@qq.com>
2017-08-21 10:11:21 +0800

    first blood!

---
+++ cmake-build-debug/CMakeFiles/simple_vehicle_counting.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pingan/simple_vehicle_counting/package_analysis/VehicleCouting.cpp" "/home/pingan/simple_vehicle_counting/cmake-build-debug/CMakeFiles/simple_vehicle_counting.dir/package_analysis/VehicleCouting.cpp.o"
+  "/home/pingan/simple_vehicle_counting/package_bgs/PBAS/PBAS.cpp" "/home/pingan/simple_vehicle_counting/cmake-build-debug/CMakeFiles/simple_vehicle_counting.dir/package_bgs/PBAS/PBAS.cpp.o"
+  "/home/pingan/simple_vehicle_counting/package_bgs/PBAS/PixelBasedAdaptiveSegmenter.cpp" "/home/pingan/simple_vehicle_counting/cmake-build-debug/CMakeFiles/simple_vehicle_counting.dir/package_bgs/PBAS/PixelBasedAdaptiveSegmenter.cpp.o"
+  "/home/pingan/simple_vehicle_counting/package_tracking/BlobTracking.cpp" "/home/pingan/simple_vehicle_counting/cmake-build-debug/CMakeFiles/simple_vehicle_counting.dir/package_tracking/BlobTracking.cpp.o"
+  "/home/pingan/simple_vehicle_counting/package_tracking/cvblob/cvaux.cpp" "/home/pingan/simple_vehicle_counting/cmake-build-debug/CMakeFiles/simple_vehicle_counting.dir/package_tracking/cvblob/cvaux.cpp.o"
+  "/home/pingan/simple_vehicle_counting/package_tracking/cvblob/cvblob.cpp" "/home/pingan/simple_vehicle_counting/cmake-build-debug/CMakeFiles/simple_vehicle_counting.dir/package_tracking/cvblob/cvblob.cpp.o"
+  "/home/pingan/simple_vehicle_counting/package_tracking/cvblob/cvcolor.cpp" "/home/pingan/simple_vehicle_counting/cmake-build-debug/CMakeFiles/simple_vehicle_counting.dir/package_tracking/cvblob/cvcolor.cpp.o"
+  "/home/pingan/simple_vehicle_counting/package_tracking/cvblob/cvcontour.cpp" "/home/pingan/simple_vehicle_counting/cmake-build-debug/CMakeFiles/simple_vehicle_counting.dir/package_tracking/cvblob/cvcontour.cpp.o"
+  "/home/pingan/simple_vehicle_counting/package_tracking/cvblob/cvlabel.cpp" "/home/pingan/simple_vehicle_counting/cmake-build-debug/CMakeFiles/simple_vehicle_counting.dir/package_tracking/cvblob/cvlabel.cpp.o"
+  "/home/pingan/simple_vehicle_counting/package_tracking/cvblob/cvtrack.cpp" "/home/pingan/simple_vehicle_counting/cmake-build-debug/CMakeFiles/simple_vehicle_counting.dir/package_tracking/cvblob/cvtrack.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/opencv"
+  "../"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
